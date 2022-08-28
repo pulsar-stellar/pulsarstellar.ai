4dc22af6ad4163e50596d4e9c4a0a4853b536100
Adrian Blanc <adrian@adrianblanc.live>
2022-08-27 19:39:22 -0500

    some changes to main app

--- pulsar/apps/core/urls.py
+++ pulsar/apps/core/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
 	path('', views.core_home, name="core-home"),
 	path('about/', views.core_about, name="core-about"),
+	path('research/', views.core_research, name="core-research"),
 ]
 
 
